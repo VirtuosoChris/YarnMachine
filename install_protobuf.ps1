88ef81aa28d99d4d0cecc02dcf80a5e20504fade
Christopher Pugh <chris@virtuosoengine.com>
2025-07-07 23:23:39 -0700

    broken.

--- install_protobuf.ps1
+++ install_protobuf.ps1
@@ -3,15 +3,17 @@ $ProtobufSrc     = Join-Path $Root "depends/protobuf"
 $ProtobufBuild   = Join-Path $Root "depends/protobuf-build"
 $ProtobufInstall = Join-Path $Root "depends/protobuf-sdk"
 
-# Configure once
+# Configure once for all configs
 cmake -S $ProtobufSrc -B $ProtobufBuild `
   -DCMAKE_INSTALL_PREFIX="$ProtobufInstall" `
   -Dprotobuf_BUILD_TESTS=OFF `
   -Dprotobuf_MSVC_STATIC_RUNTIME=OFF `
   -DCMAKE_GENERATOR_PLATFORM=x64
 
-# Build & install Debug
-cmake --build $ProtobufBuild --config Debug --target install
+# Build Debug and install to subdir
+cmake --build $ProtobufBuild --config Debug
+# Build Release and install to subdir
+cmake --build $ProtobufBuild --config Release
 
-# Build & install Release
-cmake --build $ProtobufBuild --config Release --target install
+cmake --install $ProtobufBuild  --prefix $ProtobufInstall/Debug --config Debug
+cmake --install $ProtobufBuild  --prefix $ProtobufInstall/Release --config Release
