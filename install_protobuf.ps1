429ede566f262a819752c8f3e3dae0a7753071dc
Christopher Pugh <chris@virtuosoengine.com>
2025-07-07 23:46:08 -0700

    worrrking????

--- install_protobuf.ps1
+++ install_protobuf.ps1
@@ -4,16 +4,16 @@ $ProtobufBuild   = Join-Path $Root "depends/protobuf-build"
 $ProtobufInstall = Join-Path $Root "depends/protobuf-sdk"
 
 # Configure once for all configs
+# Set CMAKE_INSTALL_PREFIX here, not in the install command
 cmake -S $ProtobufSrc -B $ProtobufBuild `
-  -DCMAKE_INSTALL_PREFIX="$ProtobufInstall" `
+  -DCMAKE_INSTALL_PREFIX="$ProtobufInstall" `  # Set the install prefix during configuration
   -Dprotobuf_BUILD_TESTS=OFF `
   -Dprotobuf_MSVC_STATIC_RUNTIME=OFF `
-  -DCMAKE_GENERATOR_PLATFORM=x64
+  -DCMAKE_GENERATOR_PLATFORM=x64 `
+  -DCMAKE_DEBUG_POSTFIX=d  # This is the key change
 
-# Build Debug and install to subdir
-cmake --build $ProtobufBuild --config Debug
-# Build Release and install to subdir
-cmake --build $ProtobufBuild --config Release
+# Build Debug and install
+cmake --build $ProtobufBuild --config Debug --target install
 
-cmake --install $ProtobufBuild  --prefix $ProtobufInstall/Debug --config Debug
-cmake --install $ProtobufBuild  --prefix $ProtobufInstall/Release --config Release
+# Build Release and install
+cmake --build $ProtobufBuild --config Release --target install
